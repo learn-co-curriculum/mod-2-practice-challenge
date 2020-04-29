490e300d5eaa9d869d11ce70cd841608c5f295a0
Rupa <rupa211984@gmail.com>
2020-04-29 14:04:16 -0700

    Validate musician in band uniqueness

--- app/models/band_member.rb
+++ app/models/band_member.rb
@@ -1,5 +1,6 @@
 class BandMember < ApplicationRecord
   validates :instrument, length: { minimum: 3 }
+  validates :musician_id, uniqueness: { scope: :band_id, message: 'can only be listed in the same band once' }
 
   belongs_to :band
   belongs_to :musician
